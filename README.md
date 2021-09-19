15d4183573e9409f5f8cd9e36290e735e593f501
Chris Dalke <chrisdalke@gmail.com>
2021-09-18 21:43:40 -0400

    Move around folder structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# cambot
+# ROS Line Following Robot
+
+
 Hardware &amp; software for a Raspberry-Pi wheeled camera robot
+
+## Dependencies
+
+- ROS
+- ROSSerial Python
+- OpenCV 2
--- frontend/src/components/ui/CardHeading/CardHeading.js
+++
@@ -1,9 +0,0 @@
-import './CardHeading.scss';
-
-function CardHeading({ title }) {
-    return (<div className="CardHeading">
-        {title}
-    </div>);
-}
-
-export default CardHeading;
